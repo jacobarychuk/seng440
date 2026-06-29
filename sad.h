2aaffe64d0af9bd4e3988b2c7e3e671f1cc865be
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2026-06-28 17:36:51 -0700

    refactor SAD computation into compute_sad function

--- sad.h
+++ sad.h
@@ -1,15 +1,19 @@
-int A[16][16], B[16][16], diff, sad = 0;
-int i, j;
+int compute_sad(int A[16][16], int B[16][16]) {
+  int diff, sad = 0;
+  int i, j;
 
-for (i=0; i<16; i++) {
-  for ( j=0; j<16; j++) {
-    diff = A[x+i][y+j] - B[(x+r)+i][(y+s)+j];
-    
-    if (diff < 0) {
-      sad -= diff;
-    }
-    else {
-      sad += diff;
+  for (i=0; i<16; i++) {
+    for ( j=0; j<16; j++) {
+      diff = A[x+i][y+j] - B[(x+r)+i][(y+s)+j];
+      
+      if (diff < 0) {
+        sad -= diff;
+      }
+      else {
+        sad += diff;
+      }
     }
   }
+
+  return sad;
 }
