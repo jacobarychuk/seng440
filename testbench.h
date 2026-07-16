de82fa5f8cb12a84aacb648cbe909c1555a80c33
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2026-07-16 14:00:20 -0700

    Implement run_testbench function

--- testbench.h
+++ testbench.h
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+void run_testbench (
+  int current_frame[240][320],
+  int reference_frame[240][320]
+) {
+  int x, y = 0;
+  int r, s = 0;
+
+  int result_original = compute_sad_original(current_frame, reference_frame, x, y, r, s);
+  printf("Original (Unoptimized) SAD: %d\n", result_original);
+}
