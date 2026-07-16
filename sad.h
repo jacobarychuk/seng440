0a4310431ad2d9d4787b3a010e44671d0ca314c7
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2026-07-16 13:57:44 -0700

    Modify compute_sad_original to use frames

--- sad.h
+++ sad.h
@@ -1,4 +1,11 @@
-int compute_sad_original(int A[16][16], int B[16][16], int x, int y, int r, int s) {
+int compute_sad_original (
+  int A[240][320], 
+  int B[240][320], 
+  int x, 
+  int y, 
+  int r, 
+  int s
+) {
   int diff, sad = 0;
   int i, j;
 
