3dc1427923fa900356e3d239dddb11bd6c59367d
Jacob Arychuk <156041293+jacobarychuk@users.noreply.github.com>
2026-07-16 19:08:22 -0700

    Refactor compute_sad_original with constants

--- sad.h
+++ sad.h
@@ -1,8 +1,12 @@
 #include <stdint.h>
 
+#define IMAGE_WIDTH 320
+#define IMAGE_HEIGHT 240
+#define BLOCK_SIZE 16
+
 int compute_sad_original (
-  uint8_t A[240][320], 
-  uint8_t B[240][320], 
+  uint8_t A[IMAGE_HEIGHT][IMAGE_WIDTH], 
+  uint8_t B[IMAGE_HEIGHT][IMAGE_WIDTH], 
   int x, 
   int y, 
   int r, 
@@ -11,8 +15,8 @@ int compute_sad_original (
   int diff, sad = 0;
   int i, j;
 
-  for (i=0; i<16; i++) {
-    for ( j=0; j<16; j++) {
+  for (i=0; i<BLOCK_SIZE; i++) {
+    for ( j=0; j<BLOCK_SIZE; j++) {
       diff = A[x+i][y+j] - B[(x+r)+i][(y+s)+j];
       
       if (diff < 0) {
